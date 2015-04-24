906f2ba89675da139d379d29f68f9b396ed5f227
Christopher De Cairos <chris@chrisdecairos.ca>
2015-04-24 11:45:44 -0400

    indexes

--- migrations/create_skittles_table.sql
+++ migrations/create_skittles_table.sql
@@ -2,5 +2,9 @@ CREATE TABLE skittles
 (
   id serial NOT NULL,
   color varchar(64) NOT NULL,
+  user_id integer NOT NULL,
   CONSTRAINT id PRIMARY KEY (id)
 );
+
+CREATE INDEX  skittles_id_index ON skittles (id);
+CREATE INDEX  skittles_id_user_index ON skittles (id, user_id);
