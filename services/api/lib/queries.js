b25420bc1c1a12906f4d34d8c406394e699a108b
Christopher De Cairos <chris@chrisdecairos.ca>
2015-05-01 15:59:44 -0400

    use a DB trigger to update update_at

--- services/api/lib/queries.js
+++ services/api/lib/queries.js
@@ -3,8 +3,8 @@ module.exports = {
     create: "INSERT INTO users (username, language, country) VALUES($1, $2, $3) RETURNING id;",
     find: "SELECT id, username, created_at, updated_at, language, country, moderator, staff " +
       "FROM users WHERE deleted_at IS NULL AND id = $1;",
-    update: "UPDATE users SET(username, language, country, updated_at) = " +
-      "($1, $2, $3, CURRENT_TIMESTAMP) WHERE deleted_at IS NULL AND id = $4 RETURNING username, language, country;",
-    remove: "UPDATE users SET(updated_at, deleted_at) = (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) WHERE id = $1;"
+    update: "UPDATE users SET(username, language, country) = " +
+      "($1, $2, $3) WHERE deleted_at IS NULL AND id = $4 RETURNING username, language, country;",
+    remove: "UPDATE users SET(deleted_at) = (CURRENT_TIMESTAMP) WHERE id = $1;"
   }
 };
