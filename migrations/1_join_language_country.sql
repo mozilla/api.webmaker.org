b01c2a2c8cba4e0331d7e0a9a1358564246ada91
Christopher De Cairos <cade@mozillafoundation.org>
2015-07-24 18:35:45 -0400

    Migration script for languages

---
+++ migrations/1_join_language_country.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+UPDATE users
+SET language = concat(users.language, '-', users.country);
+
+ALTER TABLE users
+DROP COLUMN country;
+
+ALTER TABLE users
+ALTER COLUMN language
+SET DEFAULT 'en-US';
+
+COMMIT;
