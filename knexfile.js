940e96eaca53b2ef861552b469fd10d90417e34b
ag_dubs <ashley666ashley@gmail.com>
2015-07-29 14:06:56 -0700

    add production and testing envs to knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,25 @@ const path = require('path');
 module.exports = {
   development: {
     client: 'pg',
-    debug: process.env.DEBUG == true,
+    debug: true,
+    connection: 'postgresql://localhost:5432/webmaker',
+    directory: './migrations',
+    migrations: {
+      tableName: 'migrations'
+    }
+  },
+  testing: {
+    client: 'pg',
+    debug: true,
+    connection: 'postgresql://localhost:5432/webmaker-testing',
+    directory: './migrations',
+    migrations: {
+      tableName: 'migraitons'
+    }
+  },
+  production: {
+    client: 'pg',
+    debug: false,
     connection: 'postgresql://localhost:5432/webmaker',
     directory: './migrations',
     migrations: {
