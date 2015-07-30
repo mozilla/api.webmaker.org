15079fb293a161b4920f4d773337ddcf21851763
ag_dubs <ashley666ashley@gmail.com>
2015-07-30 16:32:05 -0700

    fix transaction implementation in 04 down migration

--- migrations/04_join_country_language_columns.js
+++ migrations/04_join_country_language_columns.js
@@ -12,12 +12,14 @@ exports.up = function(knex, Promise) {
   });
 };
 
-exports.down = function(knex) {
-  return knex('users').transacting(trx)
-    //jscs:disable
-    .update('language', knex.raw("subtring(users.language, 1, 2)"))
-    .then(function() {
-      return knex.raw('ALTER TABLE "users" ALTER COLUMN "language" DROP DEFAULT');
-    })
-    .then(trx.commit);
+exports.down = function(knex, Promise) {
+  return knex.transation(function(trx) {
+    return knex('users').transacting(trx)
+      //jscs:disable
+      .update('language', knex.raw("subtring(users.language, 1, 2)"))
+      .then(function() {
+        return knex.raw('ALTER TABLE "users" ALTER COLUMN "language" DROP DEFAULT');
+      })
+      .then(trx.commit);
+  });
 };
