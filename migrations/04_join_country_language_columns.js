5ea701421119e2c64f63bd592ce340a75e096b87
ag_dubs <ashley666ashley@gmail.com>
2015-07-30 14:57:08 -0700

    pass hinting and style checks

--- migrations/04_join_country_language_columns.js
+++ migrations/04_join_country_language_columns.js
@@ -3,12 +3,13 @@
 exports.up = function(knex, Promise) {
   return knex.transaction(function(trx) {
     return knex('users').transacting(trx)
+      //jscs:disable
       .update('language', knex.raw("concat(users.language, '-', users.country)"))
       .then(function() {
         return knex.raw('ALTER TABLE "users" ALTER COLUMN "language" SET DEFAULT \'en-US\'');
       })
       .then(trx.commit);
-  })
+  });
 };
 
 exports.down = function(knex) {
