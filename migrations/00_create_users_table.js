31f5d0602b550d9384a88217e108a42f3e71d75b
ag_dubs <ashley666ashley@gmail.com>
2015-07-30 16:24:21 -0700

    add conditional create to users table migration

--- migrations/00_create_users_table.js
+++ migrations/00_create_users_table.js
@@ -1,17 +1,21 @@
 'use strict';
 
 exports.up = function(knex) {
-  return knex.schema.createTable('users', function(t) {
-    t.bigIncrements('id').notNullable();
-    t.string('username').unique().notNullable();
-    t.string('language').notNullable().defaultTo('en');
-    t.string('country').notNullable().defaultTo('US');
-    t.boolean('moderator').notNullable().defaultTo(false);
-    t.boolean('staff').notNullable().defaultTo(false);
+  knex.schema.hasTable('users').then(function(exists) {
+      if(!exists) {
+        return knex.schema.createTable('users', function(t) {
+          t.bigIncrements('id').notNullable();
+          t.string('username').unique().notNullable();
+          t.string('language').notNullable().defaultTo('en');
+          t.string('country').notNullable().defaultTo('US');
+          t.boolean('moderator').notNullable().defaultTo(false);
+          t.boolean('staff').notNullable().defaultTo(false);
 
-    t.timestamp('deleted_at').defaultTo(null);
-    t.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
-    t.timestamp('updated_at').notNullable().defaultTo(knex.raw('now()'));
+          t.timestamp('deleted_at').defaultTo(null);
+          t.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
+          t.timestamp('updated_at').notNullable().defaultTo(knex.raw('now()'));
+        });
+    }
   });
 };
 
