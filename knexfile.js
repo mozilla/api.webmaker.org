0d5f09c31c52d2ab0b18d0727b9f4b5f3bb8f138
ag_dubs <ashley666ashley@gmail.com>
2015-07-30 14:49:04 -0700

    remove all envs except development

--- knexfile.js
+++ knexfile.js
@@ -10,23 +10,5 @@ module.exports = {
     migrations: {
       tableName: 'migrations'
     }
-  },
-  testing: {
-    client: 'pg',
-    debug: true,
-    connection: process.env.POSTGRE_CONNECTION_STRING,
-    directory: './migrations',
-    migrations: {
-      tableName: 'migraitons'
-    }
-  },
-  production: {
-    client: 'pg',
-    debug: false,
-    connection: process.env.POSTGRE_CONNECTION_STRING,
-    directory: './migrations',
-    migrations: {
-      tableName: 'migrations'
-    }
   }
 };
