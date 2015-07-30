b84c24b571437df57833072fe0a74dd00c9076ce
ag_dubs <ashley666ashley@gmail.com>
2015-07-30 16:31:48 -0700

    fix hinting and style in migration 00

--- migrations/00_create_users_table.js
+++ migrations/00_create_users_table.js
@@ -2,19 +2,19 @@
 
 exports.up = function(knex) {
   knex.schema.hasTable('users').then(function(exists) {
-      if(!exists) {
-        return knex.schema.createTable('users', function(t) {
-          t.bigIncrements('id').notNullable();
-          t.string('username').unique().notNullable();
-          t.string('language').notNullable().defaultTo('en');
-          t.string('country').notNullable().defaultTo('US');
-          t.boolean('moderator').notNullable().defaultTo(false);
-          t.boolean('staff').notNullable().defaultTo(false);
+    if (!exists) {
+      return knex.schema.createTable('users', function(t) {
+        t.bigIncrements('id').notNullable();
+        t.string('username').unique().notNullable();
+        t.string('language').notNullable().defaultTo('en');
+        t.string('country').notNullable().defaultTo('US');
+        t.boolean('moderator').notNullable().defaultTo(false);
+        t.boolean('staff').notNullable().defaultTo(false);
 
-          t.timestamp('deleted_at').defaultTo(null);
-          t.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
-          t.timestamp('updated_at').notNullable().defaultTo(knex.raw('now()'));
-        });
+        t.timestamp('deleted_at').defaultTo(null);
+        t.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
+        t.timestamp('updated_at').notNullable().defaultTo(knex.raw('now()'));
+      });
     }
   });
 };
