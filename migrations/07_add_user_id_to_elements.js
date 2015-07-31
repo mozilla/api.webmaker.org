fec3635098e265199a54e359ef86a3abb9d12675
ag_dubs <ashley666ashley@gmail.com>
2015-07-31 11:28:24 -0700

    fix copy pasta pages ref in add user_id to elements in migraiton 07

--- migrations/07_add_user_id_to_elements.js
+++ migrations/07_add_user_id_to_elements.js
@@ -1,11 +1,11 @@
 exports.up = function(knex) {
-  return knex.schema.table('pages', function(t) {
+  return knex.schema.table('elements', function(t) {
     t.integer('user_id').references('id').inTable('users');
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.table('pages', function(t) {
+  return knex.schema.table('elements', function(t) {
     t.dropColumn('user_id');
   });
 };
